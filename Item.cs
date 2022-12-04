2b5e0adfff4e1745529570376eadffaad8207b07
aka8073 <aka8073@naver.com>
2022-12-04 18:54:10 +0900

    modify Item.cs by aka8073

--- Item.cs
+++ Item.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Test : MonoBehaviour 
 public class Item : MonoBehaviour
 {
+    public float a;
     public string type;
     Rigidbody2D riged;
 
